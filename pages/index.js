566563f4464232946ebd3887cfd8004a6e50f1fd
Ankit <ankit60.av@gmail.com>
2023-04-02 20:45:57 +0530

    redifined

--- pages/index.js
+++ pages/index.js
@@ -7,17 +7,18 @@ export default function Home() {
     >
       <h1 className="p-4 text-center font-bold text-[2.5rem]">BlockGPT</h1>
 
-      <div className="flex flex-col p-2 space-y-2 w-3/4 text-center">
-        <div className="md:w-2/4 mx-auto">
+      <div className="flex flex-col p-2 space-y-6  w-3/4 text-center">
+        <div className="md:w-2/4 w-full mx-auto">
           <input
             className="bg-white p-2 w-full border text-black"
             type="text"
           />
         </div>
-        <p className="font-bold text-lg">
-          Search Anything and Everything related to
-          <br /> Blockchain
-        </p>
+        <div className="md:w-2/5 mx-auto">
+          <p className="font-bold text-sm md:text-lg">
+            Search Anything and Everything related to Blockchain
+          </p>
+        </div>
         <div className="flex flex-wrap gap-2 p-2 mx-auto text-center">
           <Chrip title="Show me latest transaction vitalik.eth" />
           <Chrip title="what is alchemy api" />
